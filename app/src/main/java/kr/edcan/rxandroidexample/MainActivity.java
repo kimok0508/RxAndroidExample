f4b1256e80c4e0f3b7110de4e828830b715acf35
suhan <boludo@daum.net>
2017-06-28 13:47:27 +0900

    hello wolrd

---
+++ app/src/main/java/kr/edcan/rxandroidexample/MainActivity.java
@@ -0,0 +1,24 @@
+package kr.edcan.rxandroidexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.internal.operators.observable.ObservableAll;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textHello = (TextView) findViewById(R.id.text_hello);
+
+        Observable.just(textHello.getText().toString())
+                .map(str -> str + " Rx!")
+                .subscribe(text -> textHello.setText(text));
+    }
+}
